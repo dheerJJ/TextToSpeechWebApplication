886cc9ea2b059ccd03458148530ef625baa75859
dheerajjkumawat <dheerajjkumawat@gmai.com>
2026-07-18 20:40:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const text = document.getElementById("textToConvert");
+const convertBtn = document.getElementById("convertBtn");
+
+convertBtn.addEventListener('click', function () {
+    const speechSynch = window.speechSynthesis;
+    const enteredText = text.value;
+    const error = document.querySelector('.error-para')
+
+    if (!speechSynch.speaking && !enteredText.trim().length) {
+        error.textContent = `Nothing to Convert!!!
+        Enter text in the text area...`
+    }
+
+    if (!speechSynch.speaking && enteredText.trim().length) {
+        error.textContent = "";
+        const newUtter = new SpeechSynthesisUtterance(enteredText);
+
+        speechSynch.speak(newUtter);
+        convertBtn.textContent = "Sound is Playing...";
+
+        newUtter.onend = function () {
+            convertBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+        }
+
+        newUtter.onerror = function () {
+            convertBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+        }
+
+    }
+
+    setTimeout(() => {
+        convertBtn.textContent = "Play Converted Sound"
+    }, 5000);
+});
