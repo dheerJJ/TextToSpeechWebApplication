288bc460092da1806f007e0e798adab210ae8a9e
dheerajjkumawat <dheerajjkumawat@gmai.com>
2026-07-18 21:03:38 +0530

    play-button-bug fixing

--- index.js
+++ index.js
@@ -4,31 +4,27 @@ const convertBtn = document.getElementById("convertBtn");
 convertBtn.addEventListener('click', function () {
     const speechSynch = window.speechSynthesis;
     const enteredText = text.value;
-    const error = document.querySelector('.error-para')
+    const error = document.querySelector('.error-para');
 
     if (!speechSynch.speaking && !enteredText.trim().length) {
-        error.textContent = `Nothing to Convert!!!
-        Enter text in the text area...`
+        error.textContent = "Nothing to Convert!!! Enter text in the text area...";
+        return;
     }
 
     if (!speechSynch.speaking && enteredText.trim().length) {
         error.textContent = "";
         const newUtter = new SpeechSynthesisUtterance(enteredText);
 
-        speechSynch.speak(newUtter);
         convertBtn.textContent = "...";
+
+        speechSynch.speak(newUtter);
 
         newUtter.onend = function () {
             convertBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
-        }
+        };
 
         newUtter.onerror = function () {
             convertBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
-        }
-
+        };
     }
-
-    setTimeout(() => {
-        convertBtn.textContent = '<i class="fa-solid fa-play"></i>'
-    }, 5000);
 });
